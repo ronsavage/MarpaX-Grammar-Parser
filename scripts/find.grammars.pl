b296c76ed97b7bf12c01f83bd0a90b351d112b0f
ronsavage <ron@savage.net.au>
2013-08-19 14:15:17 +1000

    Start switching to File::ShareDir

---
+++ scripts/find.grammars.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use File::ShareDir;
+
+# --------------
+
+my($app_name) = 'MarpaX-Grammar-Parser';
+my($bnf_name) = 'stringparser.1.bnf';
+my($path)     = File::ShareDir::dist_file($app_name, $bnf_name);
+
+print "Using: File::ShareDir::dist_file('$app_name', '$bnf_name'): \n";
+print "Found: $path\n";
