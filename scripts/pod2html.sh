a260c7cb6e746f2610d82bca55afcd461b61b164
ronsavage <ron@savage.net.au>
2014-10-20 11:45:09 +1100

    Remove Utils from scripts/pod2html.sh

--- scripts/pod2html.sh
+++ scripts/pod2html.sh
@@ -3,9 +3,8 @@
 NAME=MarpaX/Grammar/Parser
 export NAME
 
-pod2html.pl -i lib/$NAME.pm       -o /dev/shm/html/Perl-modules/html/$NAME.html
-pod2html.pl -i lib/$NAME/Utils.pm -o /dev/shm/html/Perl-modules/html/$NAME/Utils.html
-
+pod2html.pl -i lib/$NAME.pm -o /dev/shm/html/Perl-modules/html/$NAME.html
+p
 NAME=Data/TreeDumper/Renderer/Marpa
 
 pod2html.pl -i lib/$NAME.pm -o /dev/shm/html/Perl-modules/html/$NAME.html
