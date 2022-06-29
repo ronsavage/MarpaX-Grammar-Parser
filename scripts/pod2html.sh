75ffae833aa4179240ac0d0689878d8501f73228
Ron Savage <ron@savage.net.au>
2022-06-29 16:17:31 +1000

    Add a comment to scripts/pod2html.sh

--- scripts/pod2html.sh
+++ scripts/pod2html.sh
@@ -3,6 +3,9 @@
 PREFIX=Perl-modules/html/MarpaX/Grammar/
 FILE=$PREFIX/Parser.html
 
+# DR is doc_root. I run Debian, and DR's value is /run/shm/html.
+# I work in a ramdisk and copy the result to hardisk (nowadays also made of ram chips).
+
 mkdir -p $DR/$PREFIX
 mkdir -p ~/savage.net.au/$PREFIX
 
